8a8e629eef676967d271d6c752af146a59a8fa95
Luke Cartledge <luke@lukecartledge.com>
2019-07-18 21:36:46 +0200

    adds git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,11 +14,14 @@ alias gp='git push origin HEAD'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit -m '
+alias gca='git commit -Am '
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
-alias ge='git-edit-new'
+alias gd='git diff'
+alias gk='gitk --all&'
+alias gx='gitx --all'
+alias gp='git push'
