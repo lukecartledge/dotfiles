57f04c18030c490d97be35f922a5aa8c131c0d60
Luke Cartledge <luke@lukecartledge.com>
2019-07-18 20:36:37 +0200

    adds some useful directory movement aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# slightly quicker jump up levels in ZSH
+alias ...='../..'
+alias ....='../../..'
+alias .....='../../../..'
