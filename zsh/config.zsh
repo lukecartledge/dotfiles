71743b36095742c850a909ea23d83b76b482658e
Luke Cartledge <luke@lukecartledge.com>
2019-08-23 08:40:30 +0200

    better grouping of options for zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,38 +1,35 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
+export UPDATE_ZSH_DAYS=7
+
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
 fpath=(/usr/local/share/zsh-completions $fpath)
 fpath=($DOTFILES/functions $fpath)
 
 autoload -U $DOTFILES/functions/*(:t)
 
+# History options
 HIST_STAMPS="dd/mm/yyyy"
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY # adds history incrementally
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt HIST_VERIFY
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
-setopt NO_SHARE_HISTORY
-unsetopt SHARE_HISTORY
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY # adds history incrementally
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
