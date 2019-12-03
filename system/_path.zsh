3bce6cb52d7f69c9dce3051fcdde28d1793f6626
lukecartledge <luke@lukecartledge.com>
2019-12-03 16:59:42 +0100

    Add Openssl installed from Homebrew

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/openssl@1.1/bin:/usr/local/opt/curl/bin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
