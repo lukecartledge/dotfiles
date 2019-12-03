f8dcf4f9623b8e3cc8c119fdc05a30ffa8e99e80
lukecartledge <luke@lukecartledge.com>
2019-12-03 17:00:35 +0100

    Adds aliases for ElasticBeanstalk
    Requires a couple of local ENV variables to be set

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,3 +10,8 @@ alias .....='../../../..'
 alias cat='bat'
 alias ping='prettyping --nolegend'
 alias top="sudo htop" # alias top and fix high sierra bug
+
+eb-ssh-staging () { eb ssh $(eb list | grep staging | sed  "s/\*//") ; }
+eb-ssh-production () { eb ssh $(eb list | grep production | sed  "s/\*//") ; }
+eb-ssh-tunnel-to-db-production () { eb ssh $(eb list | grep staging | sed  "s/\*//") --custom 'ssh -i ~/.ssh/aws-eb   -N -L "${ON_PRODUCTION_DB_URI}"' ; }
+eb-ssh-tunnel-to-db-staging () { eb ssh $(eb list | grep staging | sed  "s/\*//") --custom 'ssh -i ~/.ssh/aws-eb   -N -L "${ON_STAGING_DB_URI}"' ; }
