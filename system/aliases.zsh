e9cc8ef34b8acd3747019ba0f8920b10a8262101
lukecartledge <luke@lukecartledge.com>
2020-06-23 11:21:57 +0200

    Updates to ls with colorls

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,10 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
+# colorls overides for ls
+#   `gem install colorls`
+# Needs to be done for each version of Ruby running
+if $(colorls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias ls="colorls -h --group-directories-first -1"
+  alias l="colorls --group-directories-first --almost-all"
+  alias ll="colorls --group-directories-first --almost-all -l"
+  alias la="colorls --group-directories-first"
 fi
