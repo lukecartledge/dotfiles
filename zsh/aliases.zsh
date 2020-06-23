e78a45aa518f2f73504e5a7587734d18ac0d2618
lukecartledge <luke@lukecartledge.com>
2020-06-23 11:18:13 +0200

    Add aliases for on ngrok with apple pay testing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,3 +15,6 @@ eb-ssh-staging () { eb ssh $(eb list | rg staging | sed  "s/\*//") ; }
 eb-ssh-production () { eb ssh $(eb list | rg production | sed  "s/\*//") ; }
 eb-ssh-tunnel-to-db-production () { eb ssh $(eb list | grep production | sed  "s/\*//") --custom 'ssh -i ~/.ssh/aws-eb   -N -L "${ON_PRODUCTION_DB_URI}"' ; }
 eb-ssh-tunnel-to-db-staging () { eb ssh $(eb list | grep staging | sed  "s/\*//") --custom 'ssh -i ~/.ssh/aws-eb   -N -L "${ON_STAGING_DB_URI}"' ; }
+
+alias on-ngrok-applepay='ngrok http 3001 -subdomain on-dev-applepay'
+alias on-test-applepay='HOST=on-dev-applepay.ngrok.io LOCAL_TEST=true gulp --no-reload'
