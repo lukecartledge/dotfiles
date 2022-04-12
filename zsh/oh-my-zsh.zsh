2f099b53e9076bf2526f318cf65bd8d19fd34cf5
lukecartledge <luke@lukecartledge.com>
2022-04-12 15:15:07 +0200

    Move to PowerLevel 10k for ZSH prompt styling

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -1,13 +1,13 @@
 export TERM="xterm-256color"
 
-ZSH_THEME="bullet-train"
-BULLETTRAIN_PROMPT_ORDER=( dir git nvm ruby )
-BULLETTRAIN_PROMPT_ADD_NEWLINE=true
-BULLETTRAIN_PROMPT_SEPARATE_LINE=true
-BULLETTRAIN_RUBY_BG=default
-BULLETTRAIN_DIR_EXTENDED=0
-BULLETTRAIN_NVM_BG=default
-BULLETTRAIN_RUBY_PREFIX='\ue21e'
-BULLETTRAIN_NVM_PREFIX='\ue718'
+ZSH_THEME="powerlevel10k/powerlevel10k"
+# BULLETTRAIN_PROMPT_ORDER=( dir git nvm ruby )
+# BULLETTRAIN_PROMPT_ADD_NEWLINE=true
+# BULLETTRAIN_PROMPT_SEPARATE_LINE=true
+# BULLETTRAIN_RUBY_BG=default
+# BULLETTRAIN_DIR_EXTENDED=0
+# BULLETTRAIN_NVM_BG=default
+# BULLETTRAIN_RUBY_PREFIX='\ue21e'
+# BULLETTRAIN_NVM_PREFIX='\ue718'
 
-plugins=(git ruby ssh-agent osx node zsh-syntax-highlighting zsh-autosuggestions)
+plugins=(git ruby ssh-agent macos node asdf zsh-syntax-highlighting zsh-autosuggestions docker docker-compose)
