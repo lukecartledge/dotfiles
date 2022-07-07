59cc0c9c0cb0192e4642aba4965da20bc5f97e36
lukecartledge <luke@lukecartledge.com>
2022-07-07 15:29:09 +0200

    Expand the path definitions here to allow for different architecture terminal sessions using Homebrew
    This is currently super important to allow for continued work on new macs

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,13 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/curl/bin:$DOTFILES/bin:/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+_ARCH=$(arch)
+PROMPT="$_ARCH $PROMPT"
+
+export PATH="/opt/homebrew/opt/openssl@1.1/bin:/usr/local/opt/curl/bin:/opt/homebrew/opt/mysql@5.7/bin:/opt/homebrew/opt/mysql-client/bin:$DOTFILES/bin:$PATH"
+
+# Requires iterm2
+if [[ "$_ARCH" == "i386" ]]; then
+ export ASDF_DATA_DIR="$HOME/.asdf_x86"
+ export PATH="/usr/local/homebrew/bin:/usr/local/homebrew/opt:$PATH"
+else
+ export PATH="/opt/homebrew/bin:/opt/homebrew/opt:$PATH"
+fi
+# export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
