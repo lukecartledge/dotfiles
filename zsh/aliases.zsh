0b76b0b2551e3c4649a6163a985b4b1a1e846bc5
lukecartledge <luke@lukecartledge.com>
2022-07-07 15:27:57 +0200

    Include some useful aliases for M1/M2 macs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,4 +17,10 @@ eb-ssh-production () { eb ssh $(eb list | rg production | sed  "s/\*//") ; }
 alias on-ngrok-applepay='ngrok http 3001 -subdomain on-dev-applepay'
 alias on-test-applepay='HOST=on-dev-applepay.ngrok.io LOCAL_TEST=true gulp --no-reload'
 
+# Helpful on an M1 Mac
+# Use to install in brew as if in x86 environment
+alias brew86="arch -x86_64 /usr/local/bin/brew"
+# Run any command in x86 environment through Rosetta
+alias leg="arch -x86_64 $@"
+
 alias sub='subl'
