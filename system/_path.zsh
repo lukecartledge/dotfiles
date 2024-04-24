d664f79b6d705962f2065d7de2b144407cffb3ff
lukecartledge <luke@lukecartledge.com>
2024-04-24 10:27:50 +0200

    Aggregated updates to my homefiles

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,14 +1,1 @@
-_ARCH=$(arch)
-PROMPT="$_ARCH $PROMPT"
-
-export PATH="/opt/homebrew/opt/openssl@1.1/bin:/usr/local/opt/curl/bin:/opt/homebrew/opt/mysql@5.7/bin:/opt/homebrew/opt/mysql-client/bin:$DOTFILES/bin:$PATH"
-
-# Requires iterm2
-if [[ "$_ARCH" == "i386" ]]; then
- export ASDF_DATA_DIR="$HOME/.asdf_x86"
- export PATH="/usr/local/homebrew/bin:/usr/local/homebrew/opt:$PATH"
-else
- export PATH="/opt/homebrew/bin:/opt/homebrew/opt:$PATH"
-fi
-
-# export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="/opt/homebrew/opt/openssl@1.1/bin:/usr/local/opt/curl/bin:/opt/homebrew/opt/mysql@5.7/bin:/opt/homebrew/opt/mysql-client/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$DOTFILES/bin:$PATH"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,25 +2,10 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 
-# slightly quicker jump up levels in ZSH
-alias ...='../..'
-alias ....='../../..'
-alias .....='../../../..'
-
 alias cat='bat'
 alias ping='prettyping --nolegend'
 alias top="sudo htop" # alias top and fix high sierra bug
 
-eb-ssh-staging () { eb ssh $(eb list | rg staging | sed  "s/\*//") ; }
-eb-ssh-production () { eb ssh $(eb list | rg production | sed  "s/\*//") ; }
-
-alias on-ngrok-applepay='ngrok http 3001 -subdomain on-dev-applepay'
-alias on-test-applepay='HOST=on-dev-applepay.ngrok.io LOCAL_TEST=true gulp --no-reload'
-
-# Helpful on an M1 Mac
-# Use to install in brew as if in x86 environment
-alias brew86="arch -x86_64 /usr/local/bin/brew"
-# Run any command in x86 environment through Rosetta
-alias leg="arch -x86_64 $@"
-
 alias sub='subl'
+
+alias docker='podman'
