c0f6bdf59fcfeb19c18df84b8f71467c4725e7d7
lukecartledge <luke@lukecartledge.com>
2024-04-24 10:27:50 +0200

    Aggregated updates to my homefiles

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -1,19 +1,9 @@
 export TERM="xterm-256color"
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
-# BULLETTRAIN_PROMPT_ORDER=( dir git nvm ruby )
-# BULLETTRAIN_PROMPT_ADD_NEWLINE=true
-# BULLETTRAIN_PROMPT_SEPARATE_LINE=true
-# BULLETTRAIN_RUBY_BG=default
-# BULLETTRAIN_DIR_EXTENDED=0
-# BULLETTRAIN_NVM_BG=default
-# BULLETTRAIN_RUBY_PREFIX='\ue21e'
-# BULLETTRAIN_NVM_PREFIX='\ue718'
 
 plugins=(
-  asdf
   bundler
-  catimg
   colored-man-pages
   direnv
   docker
