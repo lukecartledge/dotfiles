265fd9dedc01ee28351549ee3e7544044a7a5416
lukecartledge <luke@lukecartledge.com>
2024-04-24 10:27:50 +0200

    Aggregated updates to my homefiles

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -982,7 +982,7 @@
   typeset -g POWERLEVEL9K_NODENV_SOURCES=(shell local global)
   # If set to false, hide node version if it's the same as global:
   # $(nodenv version-name) == $(nodenv global).
-  typeset -g POWERLEVEL9K_NODENV_PROMPT_ALWAYS_SHOW=false
+  typeset -g POWERLEVEL9K_NODENV_PROMPT_ALWAYS_SHOW=true
   # If set to false, hide node version if it's equal to "system".
   typeset -g POWERLEVEL9K_NODENV_SHOW_SYSTEM=true
   # Custom icon.
