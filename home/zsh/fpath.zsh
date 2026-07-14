05d9af45ccb71b57d2424693af6fce5e32e86a3c
Luke Cartledge <luke@lukecartledge.com>
2026-07-14 18:37:01 +0200

    chore(zsh): remove unused sourced files
    rbenv.zsh was fully commented out (mise is the version manager); fpath.zsh duplicated the fpath entry already set in config.zsh.

--- home/zsh/fpath.zsh
+++
@@ -1,4 +0,0 @@
-# Add functions directory to fpath for custom zsh functions and completions
-if [[ -d "$DOTFILES/home/functions/zsh-functions" ]]; then
-  fpath=("$DOTFILES/home/functions/zsh-functions" $fpath)
-fi
